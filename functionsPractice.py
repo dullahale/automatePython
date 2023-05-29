41d50cbdb1525e0687ad3d6887ea31486bcf246b
Abdul-dev <abdullahiisa051@gmail.com>
2023-05-29 17:51:18 -0600

    Solutions to hurdles 3 and maze from udemy

---
+++ functionsPractice.py
@@ -0,0 +1,49 @@
+# keyword def
+
+#Hurdle 3 exercise answer
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     turn_left()
+#     while wall_on_right():
+#         move()
+#
+#     turn_right()
+#     move()
+#     turn_right()
+#
+#     while front_is_clear():
+#         move()
+#
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+
+
+#Maze solution
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#
+#     elif front_is_clear():
+#         move()
+#
+#     else:
+#         turn_left()
